fc0fcfd225056c929c7e702dd8b50496f1fc6517
Matthew Eskolin <matteskolin@gmail.com>
2021-10-31 11:42:09 -0500

    clean

--- LeetCodeConsole/Program.cs
+++ LeetCodeConsole/Program.cs
@@ -7,6 +7,10 @@ namespace LeetCodeConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            _279_First_BadVersion.Run();
         }
+
+
     }
 }
