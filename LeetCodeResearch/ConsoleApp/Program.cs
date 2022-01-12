dae657b018be92e7f7365b3dce26dfb15c8b2fea
Matthew Eskolin <matteskolin@gmail.com>
2022-01-12 16:10:25 -0600

    1 Two Sum

--- LeetCodeResearch/ConsoleApp/Program.cs
+++ LeetCodeResearch/ConsoleApp/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
+
+#pragma warning restore format
 namespace LeetCodeResearch
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //_279_First_BadVersion.Run();
-            _977_Squares_of_a_Sorted_Array.Run();
+            //Move Out of Here after solving 
+            var app = new App();
+//            app.TwoSum
+
         }
 
+
+
+    }
+
+    public class App
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+                //Generate Hash Table by Iterating num and storing as <Key = nums[i] -> Val = i>
+
+                var dict_val_index = new System.Collections.Generic.Dictionary<int, int>();
+                for(int i = 0; i < nums.Length; i++)
+                {
+                    
+                }
+
+            
+        }
+            
 
     }
 }
