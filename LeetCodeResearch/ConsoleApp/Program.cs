a69bf1db7e73257a525721b88f13d79099a2581e
Matthew Eskolin <matteskolin@gmail.com>
2022-01-21 14:20:15 -0600

    Problem 2

--- LeetCodeResearch/ConsoleApp/Program.cs
+++ LeetCodeResearch/ConsoleApp/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
 
 
 #pragma warning restore format
@@ -9,9 +16,24 @@ namespace LeetCodeResearch
     {
         static void Main(string[] args)
         {
-            //Move Out of Here after solving 
-            var app = new App();
-//            app.TwoSum
+
+
+
+
+for(int i = 1; i < 10000; i++)
+{
+    using (var rng = new RNGCryptoServiceProvider())
+    {
+        var bit_count = (20 * 6);
+        var byte_count = ((bit_count + 7) / 8); // rounded up
+        var bytes = new byte[byte_count];
+        rng.GetBytes(bytes);
+        Console.WriteLine( Convert.ToBase64String(bytes));
+
+        //return
+    }
+
+}
 
         }
 
@@ -24,15 +46,39 @@ namespace LeetCodeResearch
         public int[] TwoSum(int[] nums, int target)
         {
 
-                //Generate Hash Table by Iterating num and storing as <Key = nums[i] -> Val = i>
+                //Two Pass Hash Table
 
+                //(1)
+                //Generate Hash Table by Iterating num and storing as <Key = nums[i] -> Val = i>
                 var dict_val_index = new System.Collections.Generic.Dictionary<int, int>();
                 for(int i = 0; i < nums.Length; i++)
                 {
-                    
+                    if(dict_val_index.ContainsKey(nums[i]))
+                    {
+                        dict_val_index[nums[i]] = i;
+                    }   
+                    else
+                    {                        
+                        dict_val_index.Add(nums[i],i);       
+                    }                 
+                                
                 }
 
-            
+                //(2)
+                //Go through the nums array again. The Key step is to pre-compute the complement and then look for it
+                for(int i = 0; i < nums.Length; i++)
+                {
+                    var complement = target - nums[i];
+                    
+                    //must exclude current index, as we must use two values added together to get target - we can't use the same value for both addend and 
+                    if(dict_val_index.ContainsKey(complement) && dict_val_index[complement] != i)
+                    {
+                        return new int[]{i, dict_val_index[complement]};                       
+                    }
+                }
+
+                return null;
+
         }
             
 
